cb2f8eb994e23d26ee71d27fed6509b1f5847ac5
MJOBrienGH <mjobrien@gatech.edu>
2014-03-19 12:49:09 -0400

    More small changes expanded console sim, started experimental robot class

---
+++ Code/RoboSim/RoboSim/Robot/exper_robot.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import robot_arch.py
+
+class exper_robot(robot_arch):
+#	Base class provides
+#    def __init__(self, image, pos_x, pos_y, azimuth, fwd_speed, spin_speed,\
+#    def update(self, occupancy_grid, agent_list):
+#    def sense(self, occupancy_grid, agent_list):				
+#    def move(self,dx,dy):
+#    def rotate(self,dtheta):
+	
+	def test_behavior:
+		move(2,0)
+		print "moved left"
+		move(-2,0)
+		print "moved right"
+		
--- Code/RoboSim/RoboSim/consoleSim.py
+++ Code/RoboSim/RoboSim/consoleSim.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 
 class Map:
-	def __init__(self,size):	#Takes size in row/column format
+	def __init__(self,size,num_agents):	#Takes size in row/column format
 		self.grid = [[0 for i in range(size[1])] for j in range(size[0])]
+		self.agent_list = [[0,0] for i in range(num_agents)]
 	
 	def printGrid(self):
 		for i in self.grid:	#For each row i
 			for j in i:		#For each column j of row i
-				print j,	#Print that node
+				if j>=0:
+					print " - ",	#Print that node
+				else:
+					print "[ ]",
 			print ""
 		print "\n"
+		
+	def createDirt(self, top_left, bottom_right):
+		for i in range(top_left[0],bottom_right[0]):
+			for j in range(top_left[1],bottom_right[1]):
+				self.grid[i][j] = -1
 		
 
 #Just for testing
 temp_size = [10,15]
-temp_map = Map(temp_size)
-temporary_map.printGrid()
+temp_map = Map(temp_size, 2)
+temp_map.printGrid()
+temp_map.createDirt([3,0],[10,15])
+temp_map.printGrid()
